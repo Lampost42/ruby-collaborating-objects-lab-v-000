4a7674c258827dcbeac3ce19d13102e33bb377e0
Trevor Russell <truss.42@gmail.com>
2018-06-20 16:42:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,8 @@
 class Song 
   attr_accessor :artist, :name
-  
+  def initialize(name)
+    @name = name
+  end
   def self.new_by_filename(filename)
     song = self.new
     song.name = filename.split(" - ")[1]
