abf874551cf0f3bafa0fec3d15809733d03f1fdd
Trevor Russell <truss.42@gmail.com>
2018-06-20 16:51:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,11 +8,13 @@ class Song
       self.artist = Artist.new(artist_name)
     else
       self.artist.name = artist_name
+    end
+  end
   def self.new_by_filename(filename)
     file = filename.split(" - ")
     name = file[1]
     song = self.new(name)
-    song.artist = file[0]
+    song.artist_name = file[0]
     song
   end
 end
