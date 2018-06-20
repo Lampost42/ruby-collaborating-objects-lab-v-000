1dd5a8d69dddedc2fdb8495315dc8e54a12d8061
Trevor Russell <truss.42@gmail.com>
2018-06-20 16:49:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
     @name = name
   end
   def artist_name=(artist_name)
-    
+    @artist = self
   def self.new_by_filename(filename)
     file = filename.split(" - ")
     name = file[1]
