29fb422f3574cb0f48e10670d908cedf6bbc9629
Trevor Russell <truss.42@gmail.com>
2018-06-20 17:53:25 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,7 +6,7 @@ class MP3Importer
   end
   
   def files
-    file_list = Dir.entries(self.path).collect{|file| file.include?(".mp3")}
+    file_list = Dir.entries(self.path).select{|file| file.include?(".mp3")}
     file_list
   end
     binding.pry
